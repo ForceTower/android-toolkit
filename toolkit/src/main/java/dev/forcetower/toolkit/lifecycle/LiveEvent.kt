9756944761724911c2bb0adf1fd6722bce75ab24
João Sena <joaopaulo761@gmail.com>
2020-11-13 22:45:07 -0300

    Use LiveEvent as the SingleEvent name

--- toolkit/src/main/java/dev/forcetower/toolkit/lifecycle/LiveEvent.kt
+++ toolkit/src/main/java/dev/forcetower/toolkit/lifecycle/LiveEvent.kt
@@ -2,7 +2,7 @@ package dev.forcetower.toolkit.lifecycle
 
 import androidx.lifecycle.Observer
 
-open class Event<out T>(private val content: T) {
+open class LiveEvent<out T>(private val content: T) {
     private var hasBeenHandled = false
 
     fun getIfNotHandled(): T? {
@@ -17,8 +17,8 @@ open class Event<out T>(private val content: T) {
     fun peek(): T = content
 }
 
-class EventObserver<T>(private val onEventUnhandled: (T) -> Unit) : Observer<Event<T>> {
-    override fun onChanged(event: Event<T>?) {
+class EventObserver<T>(private val onEventUnhandled: (T) -> Unit) : Observer<LiveEvent<T>> {
+    override fun onChanged(event: LiveEvent<T>?) {
         event?.getIfNotHandled()?.let { value ->
             onEventUnhandled(value)
         }
