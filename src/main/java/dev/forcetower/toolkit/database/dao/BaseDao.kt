a5815e06f6b601017dde93f1f0662c5972a07887
João Paulo Sena <joaopaulo761@gmail.com>
2022-02-09 20:54:25 -0300

    Change UPSERT method

--- src/main/java/dev/forcetower/toolkit/database/dao/BaseDao.kt
+++ src/main/java/dev/forcetower/toolkit/database/dao/BaseDao.kt
@@ -8,19 +8,22 @@ import androidx.room.Update
 
 abstract class BaseDao<T> {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insert(value: T)
+    abstract suspend fun insert(value: T): Long
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    abstract suspend fun insertIgnore(value: T)
+    abstract suspend fun insertIgnore(value: T): Long
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertAll(values: List<T>)
+    abstract suspend fun insertAll(values: List<T>): List<Long>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    abstract suspend fun insertAllIgnore(values: List<T>)
+    abstract suspend fun insertAllIgnore(values: List<T>): List<Long>
 
     @Update(onConflict = OnConflictStrategy.IGNORE)
     abstract suspend fun update(value: T)
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun updateAll(value: List<T>)
 
     @Delete
     abstract suspend fun delete(values: List<T>)
@@ -30,20 +33,18 @@ abstract class BaseDao<T> {
 
     @Transaction
     open suspend fun insertOrUpdate(values: List<T>) {
-        values.forEach { value ->
-            val current = getValueByIDDirect(value)
-            if (current != null) {
-                if (current != value) update(value)
-            } else insert(value)
+        val result = insertAllIgnore(values)
+        val update = mutableListOf<T>()
+        result.forEachIndexed { index, value ->
+            if (value == -1L) update += values[index]
         }
+        if (update.isNotEmpty()) updateAll(update)
     }
 
     @Transaction
     open suspend fun insertOrUpdate(value: T) {
-        val current = getValueByIDDirect(value)
-        if (current != null) {
-            if (current != value) update(value)
-        } else insert(value)
+        val result = insertIgnore(value)
+        if (result == -1L) update(value)
     }
 
     protected open suspend fun getValueByIDDirect(value: T): T? = null
